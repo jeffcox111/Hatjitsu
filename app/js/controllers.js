44885ca945ecb8e1ab7a69e2ee45bd11f1d871f4
Unknown <jeffcox111@gmail.com>
2018-01-25 15:34:15 -0600

    Testing out adding a new card deck.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -201,6 +201,7 @@ function RoomCtrl($scope, $routeParams, $timeout, socket) {
     var seq = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '?'];
     var play = ['A\u2660', '2', '3', '5', '8', '\u2654'];
     var tshirt = ['XL', 'L', 'M', 'S', 'XS', '?'];
+    var devs = ['C.S.', 'R.J.', 'J.A.', 'R.W.', 'B.K.', 'T.P.', 'B.W.', 'M.B.']
     switch (val) {
     case ('fib'):
       return fib;
@@ -212,6 +213,8 @@ function RoomCtrl($scope, $routeParams, $timeout, socket) {
       return play;
     case ('tshirt'):
       return tshirt;
+    case ('devs'):
+      return devs;
     default:
       return [];
     }
