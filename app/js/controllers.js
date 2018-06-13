8acec16f95a3682bf0357de1efad191af407694b
Unknown <jeffcox111@gmail.com>
2018-06-13 11:32:43 -0500

    Normalized the TyxTime to all be relative to days instead of hours

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -201,7 +201,7 @@ function RoomCtrl($scope, $routeParams, $timeout, socket) {
     var seq = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '?'];
     var play = ['A\u2660', '2', '3', '5', '8', '\u2654'];
     var tshirt = ['XL', 'L', 'M', 'S', 'XS', '?'];
-    var times    = ['4h', '8h', '16h', '24h', '40h', '7d', '10d', '15d', '20d', '?', '\u2615'];
+    var times    = ['1/2d', '1d', '2d', '3d', '5d', '7d', '10d', '15d', '20d', '?', '\u2615'];
     switch (val) {
     case ('fib'):
       return fib;
